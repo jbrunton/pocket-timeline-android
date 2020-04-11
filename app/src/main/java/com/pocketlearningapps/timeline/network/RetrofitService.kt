dea9aad7748243562b795df40d69c7402ceb1d30
John Brunton <jbrunton@zipcar.co.uk>
2020-04-11 19:52:13 +0100

    LRN-22 load timelines from api

--- app/src/main/java/com/pocketlearningapps/timeline/network/RetrofitService.kt
+++ app/src/main/java/com/pocketlearningapps/timeline/network/RetrofitService.kt
@@ -2,6 +2,7 @@ package com.pocketlearningapps.timeline.network
 
 import com.google.gson.JsonElement
 import com.google.gson.annotations.SerializedName
+import com.pocketlearningapps.timeline.entities.Timeline
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Headers
@@ -23,14 +24,16 @@ interface RetrofitService {
         "Accept: application/json",
         "Content-Type: application/json"
     )
-    @POST("auth/google_oauth2/signin")
+    @POST("/auth/google_oauth2/signin")
     suspend fun validateToken(@Body request: ValidateTokenRequest): JsonElement
 
     @Headers(
         "Accept: application/json",
         "Content-Type: application/json"
     )
-    @GET("user/profile")
+    @GET("/user/profile")
     suspend fun profile(): UserResponse
-}
 
+    @GET("/timelines")
+    suspend fun timelines(): List<Timeline>
+}
--- app/src/main/java/com/pocketlearningapps/timeline/ui/timelines/TimelinesFragment.kt
+++ app/src/main/java/com/pocketlearningapps/timeline/ui/timelines/TimelinesFragment.kt
@@ -5,20 +5,33 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.jbrunton.inject.Container
+import com.jbrunton.inject.HasContainer
+import com.jbrunton.inject.inject
+import com.jbrunton.inject.injectViewModel
 import com.pocketlearningapps.timeline.R
+import com.pocketlearningapps.timeline.network.RetrofitService
 import kotlinx.android.synthetic.main.fragment_timelines.*
 
-class TimelinesFragment : Fragment(R.layout.fragment_timelines) {
+class TimelinesFragment : Fragment(R.layout.fragment_timelines), HasContainer {
+    override val container by lazy { (activity?.application as HasContainer).container }
+
+    val service: RetrofitService by inject()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         (activity as AppCompatActivity).setSupportActionBar(toolbar)
 
-        val adapter = TimelinesAdapter().apply {
-            setData(listOf(sampleTimeline))
-        }
+        val adapter = TimelinesAdapter()
         timelines.adapter = adapter
         timelines.layoutManager = LinearLayoutManager(activity)
+
+        lifecycleScope.launchWhenStarted {
+            val timelines = service.timelines()
+            adapter.setData(timelines)
+        }
     }
 }
