f2e25fe3a34b1a18454a20c0d5ca9e92c9147117
John Brunton <jbrunton@zipcar.co.uk>
2020-04-11 16:16:53 +0100

    LRN-18 add WhatDateFragment

--- app/src/main/java/com/pocketlearningapps/timeline/ui/quiz/QuizFragment.kt
+++ app/src/main/java/com/pocketlearningapps/timeline/ui/quiz/QuizFragment.kt
@@ -5,12 +5,20 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.pocketlearningapps.timeline.R
-import kotlinx.android.synthetic.main.fragment_timelines.*
+import kotlinx.android.synthetic.main.fragment_quiz.*
 
 class QuizFragment : Fragment(R.layout.fragment_quiz) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         (activity as AppCompatActivity).setSupportActionBar(toolbar)
+
+        nextQuestion()
+    }
+
+    fun nextQuestion() {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.question_content, WhatDateFragment())
+            .commit()
     }
 }
