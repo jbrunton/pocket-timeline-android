733b1839aad60ec533047bf7bfdf6ab29e05eaf6
John Brunton <jbrunton@zipcar.co.uk>
2020-04-11 21:53:57 +0100

    LRN-24 load events for WhatDateFragment from API

--- app/src/main/java/com/pocketlearningapps/timeline/ui/quiz/QuizFragment.kt
+++ app/src/main/java/com/pocketlearningapps/timeline/ui/quiz/QuizFragment.kt
@@ -4,11 +4,22 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.jbrunton.inject.Container
+import com.jbrunton.inject.HasContainer
+import com.jbrunton.inject.inject
 import com.pocketlearningapps.timeline.R
+import com.pocketlearningapps.timeline.entities.Timeline
+import com.pocketlearningapps.timeline.network.RetrofitService
 import kotlinx.android.synthetic.main.fragment_quiz.*
+import kotlinx.coroutines.launch
 import kotlin.random.Random
 
-class QuizFragment : Fragment(R.layout.fragment_quiz) {
+class QuizFragment : Fragment(R.layout.fragment_quiz), HasContainer {
+    override val container by lazy { (activity?.application as HasContainer).container }
+    private lateinit var timelines: List<Timeline>
+    private val service: RetrofitService by inject()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -16,13 +27,18 @@ class QuizFragment : Fragment(R.layout.fragment_quiz) {
 
         submit.setOnClickListener { nextQuestion() }
 
-        nextQuestion()
+        lifecycleScope.launch {
+            timelines = service.timelines()
+            nextQuestion()
+        }
     }
 
     private fun nextQuestion() {
-        val fragment = if (Random.nextBoolean()) { WhatDateFragment() } else { WhichEventFragment() }
+        val timeline = timelines.get(Random.nextInt(timelines.size))
+        val fragment = WhatDateFragment.build(timeline.id)
         requireActivity().supportFragmentManager.beginTransaction()
             .replace(R.id.question_content, fragment)
             .commit()
     }
 }
+
