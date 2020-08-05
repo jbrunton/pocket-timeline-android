c5b28bf05ecf1a0f9cecfc32c34f45bf41fec75b
John Brunton <john_brunton@hotmail.co.uk>
2020-08-05 11:20:07 +0100

    request overlay permissions

--- android/app/src/main/java/com/pocketlearningapps/timeline/ui/react/MyReactActivity.kt
+++ android/app/src/main/java/com/pocketlearningapps/timeline/ui/react/MyReactActivity.kt
@@ -1,7 +1,11 @@
 package com.pocketlearningapps.timeline.ui.react
 
 import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.view.KeyEvent
 import com.facebook.react.ReactInstanceManager
 import com.facebook.react.ReactRootView
@@ -9,27 +13,42 @@ import com.facebook.react.common.LifecycleState
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
 import com.facebook.react.shell.MainReactPackage
 import com.pocketlearningapps.timeline.BuildConfig
+import java.lang.RuntimeException
 
+private const val OVERLAY_PERMISSION_REQ_CODE = 1
 
 class MyReactActivity : Activity(), DefaultHardwareBackBtnHandler {
     private var mReactRootView: ReactRootView? = null
     private var mReactInstanceManager: ReactInstanceManager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        mReactRootView = ReactRootView(this)
-        mReactInstanceManager = ReactInstanceManager.builder()
-            .setApplication(application)
-            .setCurrentActivity(this)
-            .setBundleAssetName("index.android.bundle")
-            .setJSMainModulePath("index")
-            .addPackage(MainReactPackage())
-            .setUseDeveloperSupport(BuildConfig.DEBUG)
-            .setInitialLifecycleState(LifecycleState.RESUMED)
-            .build()
-        // The string here (e.g. "MyReactNativeApp") has to match
-        // the string in AppRegistry.registerComponent() in index.js
-        mReactRootView!!.startReactApplication(mReactInstanceManager, "MyReactNativeApp", null)
-        setContentView(mReactRootView)
+
+        if (!Settings.canDrawOverlays(this)) {
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName")
+            )
+            startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE)
+        } else {
+            startReact()
+        }
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int,
+        data: Intent?
+    ) {
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (!Settings.canDrawOverlays(this)) {
+                throw RuntimeException("Unable to debug")
+                // SYSTEM_ALERT_WINDOW permission not granted
+            } else {
+                startReact()
+            }
+        }
+        mReactInstanceManager!!.onActivityResult(this, requestCode, resultCode, data)
     }
 
     override fun invokeDefaultOnBackPressed() {
@@ -74,5 +93,22 @@ class MyReactActivity : Activity(), DefaultHardwareBackBtnHandler {
             return true
         }
         return super.onKeyUp(keyCode, event)
+    }
+
+    private fun startReact() {
+        mReactRootView = ReactRootView(this)
+        mReactInstanceManager = ReactInstanceManager.builder()
+            .setApplication(application)
+            .setCurrentActivity(this)
+            .setBundleAssetName("index.android.bundle")
+            .setJSMainModulePath("index")
+            .addPackage(MainReactPackage())
+            .setUseDeveloperSupport(BuildConfig.DEBUG)
+            .setInitialLifecycleState(LifecycleState.RESUMED)
+            .build()
+        // The string here (e.g. "MyReactNativeApp") has to match
+        // the string in AppRegistry.registerComponent() in index.js
+        mReactRootView!!.startReactApplication(mReactInstanceManager, "MyReactNativeApp", null)
+        setContentView(mReactRootView)
     }
 }
