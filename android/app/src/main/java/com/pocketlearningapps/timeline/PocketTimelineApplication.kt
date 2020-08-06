4f62cd760246a6ec2a6cc6de249a78451ce78164
John Brunton <john_brunton@hotmail.co.uk>
2020-08-06 16:58:34 +0100

    implement ReactApplication

--- android/app/src/main/java/com/pocketlearningapps/timeline/PocketTimelineApplication.kt
+++ android/app/src/main/java/com/pocketlearningapps/timeline/PocketTimelineApplication.kt
@@ -1,15 +1,29 @@
 package com.pocketlearningapps.timeline
 
 import android.app.Application
+import com.facebook.react.ReactApplication
+import com.facebook.react.ReactNativeHost
+import com.facebook.react.ReactPackage
+import com.facebook.react.shell.MainReactPackage
 import com.jbrunton.inject.Container
 import com.jbrunton.inject.HasContainer
 import com.pocketlearningapps.timeline.di.ApplicationComponent
 
-class PocketTimelineApplication : Application(), HasContainer {
+class PocketTimelineApplication : Application(), HasContainer, ReactApplication {
     override val container: Container = Container()
 
     override fun onCreate() {
         super.onCreate()
         container.register(ApplicationComponent(this))
+    }
+
+    override fun getReactNativeHost(): ReactNativeHost {
+        return object : ReactNativeHost(this) {
+            override fun getPackages(): MutableList<ReactPackage> = mutableListOf(
+                MainReactPackage()
+            )
+
+            override fun getUseDeveloperSupport() = BuildConfig.DEBUG
+        }
     }
 }
