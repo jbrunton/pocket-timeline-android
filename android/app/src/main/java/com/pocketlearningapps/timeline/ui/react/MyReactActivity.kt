257d054c38d9f6794f47b2a452e4292135bd1bbf
John Brunton <john_brunton@hotmail.co.uk>
2020-08-06 17:31:50 +0100

    adding navigation

--- android/app/src/main/java/com/pocketlearningapps/timeline/ui/react/MyReactActivity.kt
+++ android/app/src/main/java/com/pocketlearningapps/timeline/ui/react/MyReactActivity.kt
@@ -1,17 +1,12 @@
 package com.pocketlearningapps.timeline.ui.react
 
-import android.app.Activity
-import android.content.Intent
-import android.net.Uri
-import android.os.Build
-import android.os.Bundle
-import android.provider.Settings
-import android.view.KeyEvent
 import com.facebook.react.*
 import com.facebook.react.common.LifecycleState
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
 import com.facebook.react.shell.MainReactPackage
 import com.pocketlearningapps.timeline.BuildConfig
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage
+
 
 class MyReactActivity : ReactActivity(), DefaultHardwareBackBtnHandler {
     override fun getMainComponentName() = "MyReactNativeApp"
@@ -32,7 +27,8 @@ class MyReactActivity : ReactActivity(), DefaultHardwareBackBtnHandler {
             override fun getReactNativeHost(): ReactNativeHost {
                 return object : ReactNativeHost(application) {
                     override fun getPackages(): MutableList<ReactPackage> = mutableListOf(
-                        MainReactPackage()
+                        MainReactPackage(),
+                        RNGestureHandlerPackage()
                     )
 
                     override fun getUseDeveloperSupport() = BuildConfig.DEBUG
--- index.js
+++ index.js
@@ -1,34 +1,41 @@
-import React from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-import { FibApp } from 'TestReactComponentJS/App'
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { AppRegistry, Button } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-class HelloWorld extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.hello}>{message}</Text>
-      </View>
-    );
-  }
-}
-var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center'
-  },
-  hello: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  }
-});
+const Stack = createStackNavigator();
+
+const MyStack = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome' }}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <Button
+      title="Go to Jane's profile"
+      onPress={() =>
+        navigation.navigate('Profile', { name: 'Jane' })
+      }
+    />
+  );
+};
+const ProfileScreen = () => {
+  return <Text>This is Jane's profile</Text>;
+};
 
 AppRegistry.registerComponent(
   'MyReactNativeApp',
-  () => FibApp
+  () => MyStack
 );
