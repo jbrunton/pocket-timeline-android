7e188344ea8dadcc7a5047922a9c86adba34f8ca
John Brunton <john_brunton@live.co.uk>
2020-08-07 19:33:56 +0100

    use PocketTimelineReact

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ import * as React from 'react';
 import { AppRegistry, Button, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import {TimelineScreen} from 'PocketTimelineReact';
 
 const Stack = createStackNavigator();
 
-const MyStack = () => {
+const HomeScreen = ({ navigation }) => {
+  return (
+    <Button
+      title="Show Timeline"
+      onPress={() =>
+        navigation.navigate('TimelineScreen', { timelineId: "1" })
+      }
+    />
+  );
+};
+
+export default TimelineApp = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -15,27 +27,13 @@ const MyStack = () => {
           component={HomeScreen}
           options={{ title: 'Welcome' }}
         />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="TimelineScreen" component={TimelineScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
 
-const HomeScreen = ({ navigation }) => {
-  return (
-    <Button
-      title="Go to Jane's profile"
-      onPress={() =>
-        navigation.navigate('Profile', { name: 'Jane' })
-      }
-    />
-  );
-};
-const ProfileScreen = () => {
-  return <Text>This is Jane's profile</Text>;
-};
-
 AppRegistry.registerComponent(
   'MyReactNativeApp',
-  () => MyStack
+  () => TimelineApp
 );
