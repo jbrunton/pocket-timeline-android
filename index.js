95c2ed76030386bd3bca461ae21a4a4cd897bf2f
John Brunton <john_brunton@live.co.uk>
2020-08-11 14:57:43 +0100

    import TimelinesScreen

--- index.js
+++ index.js
@@ -3,43 +3,41 @@ import * as React from 'react';
 import { AppRegistry, Button, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import {TimelineScreen} from 'PocketTimelineReact';
+//import {TimelineScreen} from 'PocketTimelineReact';
+import {TimelinesScreen} from 'pocket-timeline-react-module/src/timelines/screens/timelines';
 
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
 
-export default TimelineApp = (props) => {
-  console.log("props: " + JSON.stringify(props))
-  const HomeScreen = ({ navigation }) => {
-    return (
-      <Button
-        title="Show Timeline"
-        onPress={() =>
-          navigation.navigate('TimelineScreen', { timelineId: props.timeline_id })
-        }
-      />
-    );
-  };
+// export default TimelineApp = (props) => {
+//   console.log("props: " + JSON.stringify(props))
+//   const HomeScreen = ({ navigation }) => {
+//     return (
+//       <Button
+//         title="Show Timeline"
+//         onPress={() =>
+//           navigation.navigate('TimelineScreen', { timelineId: props.timeline_id })
+//         }
+//       />
+//     );
+//   };
   
-  return (
-    <NavigationContainer>
-      <Stack.Navigator props={props}>
-        <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          props={props}
-          options={{ title: props.timeline_title }}
-        />
-        <Stack.Screen name="TimelineScreen" component={TimelineScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator props={props}>
+//         <Stack.Screen
+//           name="Home"
+//           component={HomeScreen}
+//           props={props}
+//           options={{ title: props.timeline_title }}
+//         />
+//         <Stack.Screen name="TimelineScreen" component={TimelineScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// };
 
 function Welcome(props) {
   return <Text>Hello, {props.AUDIENCE}!</Text>;
 }
 
-AppRegistry.registerComponent('ReactTest', () => TimelineApp);
-
-
-//AppRegistry.registerComponent('ReactTest', () => Welcome)
+AppRegistry.registerComponent('ReactTest', () => TimelinesScreen)
